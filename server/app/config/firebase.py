8f32fdc4e5ca9ec5e8ac146aa2af45d4d14d2c18
keshavreddyaletip <keshavreddyaleti.p@gmail.com>
2026-07-16 21:52:57 +0530

    Use environment variables for Firebase

--- server/app/config/firebase.py
+++ server/app/config/firebase.py
@@ -1,9 +1,27 @@
+import os
+import json
+
 import firebase_admin
+
 from firebase_admin import credentials, firestore
 
-# Prevent multiple initializations
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate("app/config/serviceAccountKey.json")
+
+    if os.getenv("FIREBASE_CREDENTIALS"):
+
+        cred_dict = json.loads(
+            os.getenv("FIREBASE_CREDENTIALS")
+        )
+
+        cred = credentials.Certificate(cred_dict)
+
+    else:
+
+        cred = credentials.Certificate(
+            "app/config/serviceAccountKey.json"
+        )
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
